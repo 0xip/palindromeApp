b5c59cf350d7a1856157e11da05f9cd5ba2c8b4e
odi <1nbummth@duck.com>
2024-08-12 09:33:34 +0300

    setted basic of JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
- 
+document.getElementById("check-btn").addEventListener("click", () => {
+const inputValue = document.getElementById("text-input").value;
+if(inputValue === ""){
+    alert("Please input a value");
+}    
+});
