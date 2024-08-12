32b16f6a988ed510c8cc87accc23ea801a182d29
odi <1nbummth@duck.com>
2024-08-12 11:04:31 +0300

    add script code

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 document.getElementById("check-btn").addEventListener("click", () => {
 const inputValue = document.getElementById("text-input").value;
+
+const modifiedInputValue = inputValue.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    
+const isPalindrome = modifiedInputValue === modifiedInputValue.split('').reverse().join('');
+    
+
 if(inputValue === ""){
     alert("Please input a value");
-}    
+}
+else if(isPalindrome){
+    document.getElementById("result").textContent = `${inputValue} is a palindrome!`;
+}else{
+    document.getElementById("result").textContent = `${inputValue} is not a palindrome!`;
+}
 });
